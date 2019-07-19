88ed0d0784d9788204e071d91da3b73f38cf3771
Tomas A. Cardoner <tomas@cardoner.com.ar>
2019-07-19 11:14:23 -0300

    Updates

--- SQL Scripts/Stored Procedure - PersonasPorReserva.sql
+++ SQL Scripts/Stored Procedure - PersonasPorReserva.sql
@@ -16,6 +16,7 @@ IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'usp_PersonasP
 GO
 
 CREATE PROCEDURE usp_PersonasPorReserva
+	@IDViaje int,
 	@IDViajeDetalle int,
 	@ReservaCodigo char(8),
 	@GrupoNumero tinyint
@@ -28,7 +29,10 @@ CREATE PROCEDURE usp_PersonasPorReserva
 		SELECT p.IDPersona, p.Apellido, p.Nombre, dt.Nombre AS DocumentoTipoNombre, p.DocumentoNumero
             FROM (Persona AS p INNER JOIN DocumentoTipo AS dt ON p.IDDocumentoTipo = dt.IDDocumentoTipo)
 				INNER JOIN ViajeDetalle AS vd ON p.IDPersona = vd.IDPersona
-			WHERE (@ReservaCodigo IS NOT NULL AND vd.ReservaCodigo = @ReservaCodigo)
+			WHERE vd.IDViaje = @IDViaje
+				AND ((@ReservaCodigo IS NOT NULL AND vd.ReservaCodigo = @ReservaCodigo)
+						OR (@ReservaCodigo IS NULL AND @GrupoNumero IS NOT NULL AND vd.GrupoNumero = @GrupoNumero)
+						OR (@ReservaCodigo IS NULL AND @GrupoNumero IS NULL AND vd.IDViajeDetalle = @IDViajeDetalle))
 	END
 GO
 
