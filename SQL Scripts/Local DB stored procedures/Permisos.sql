4e6e4f966bb0164655fb8ab3f76e32652fa5b914
Tomás A. Cardoner <tomas@cardoner.com.ar>
2019-11-04 23:40:00 -0300

    Permisos

---
+++ SQL Scripts/Local DB stored procedures/Permisos.sql
@@ -0,0 +1,50 @@
+USE CSTransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_VehiculoConfiguracionDetalle_ObtenerPorID TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_Empresa_ObtenerPorID TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_EventLog_Agregar TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_Imagen_ObtenerPorID TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_Kiosko_ActualizarUltimaConexion TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_Kiosko_ActualizarUltimaOperacion TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_Kiosko_ObtenerPorID TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_Kiosko_ObtenerPorMAC TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_KioskoConfiguracion_ObtenerPorID TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_KioskoConfiguracion_ObtenerValores TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_KioskoConfiguracionValor_ObtenerPorID TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_TicketPlantilla_ObtenerComandos TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_TicketPlantilla_ObtenerPorID TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_TicketPlantillaComando_ObtenerPorID TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_VehiculoConfiguracion_ObtenerDetalles TO cstransporte
+GO
+
+GRANT EXECUTE ON dbo.usp_VehiculoConfiguracion_ObtenerPorID TO cstransporte
+GO
